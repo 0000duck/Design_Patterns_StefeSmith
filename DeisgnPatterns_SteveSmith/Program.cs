473dbaedabba12ea382a6ea88c3ae200d18d4e4f
heheartur <heheartur@gmail.com>
2014-04-08 16:08:21 +0200

    singleton

---
+++ DeisgnPatterns_SteveSmith/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeisgnPatterns_SteveSmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    //Singleton Pattern
+    // CEL : +zapewnienie by klasa miała tylko 1 instancję 
+    //       +udostępnienie globalnego punktu dostępu do tej instancji  
+    //public class Singleton
+    //{
+    //    private static Singleton _instance;
+    //    private Singleton()
+    //    {
+
+    //    }
+    //    public static Singleton Instance
+    //    {
+    //        get
+    //        {
+    //            if (_instance == null)
+    //            {
+    //                _instance = new Singleton();
+    //            }
+    //            return _instance;
+    //        }
+    //    }
+    //}
+    public class Factory
+    {
+        private static Factory _factory;
+        private static Object _classLock = typeof(Factory);
+        private long _wipMoves;
+        private Factory()
+        {
+            _wipMoves = 0;
+        }
+        //
+        public static Factory GetFactory()
+        {
+            lock (_classLock)
+            {
+                if (_factory == null)
+                {
+                    _factory = new Factory();
+                }
+                return _factory;
+            }
+        }
+
+        public void RecordWipMove()
+        {
+
+            //zadanie
+        }
+    }
+}
